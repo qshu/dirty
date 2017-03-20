e504dc83d1c7d0a4b9a677aeafc231481a11d4ac
qshu <shuqi@pku.edu.cn>
2017-03-20 21:17:50 +0800

    start new branch ffc: free-floating comets

--- mcluster/generate.sh
+++ mcluster/generate.sh
@@ -5,24 +5,27 @@ make mcluster
 
 
 unit=0 # 0 --> Nbody unit;  1 --> astrophysics unit
-N=12800
+Ns=128000
+Nc=$Ns
 #B=51200
 B=0
-NAME=N12.8k_b0_comets0k
+T=1000
+NAME=Ns${Ns}_B${B}_comets${Nc}_timeNB_$T
+
 
 rm *.info *.input *.10
 
 
-./mcluster -N $N -B $B -m 0.08 -m 10 -s 12345 -t 3 -u $unit -C 5 -o $NAME > log.gen
+./mcluster -N $Ns -B $B -m 0.08 -m 10 -s 12345 -t 3 -u $unit -C 5 -T $T -o $NAME > log.gen
 var=`awk '$1=="scalingInfo" {print $4, $7}' log.gen`
 echo $var
 #python aei.py $var $NAME
 
 
-
-#./mcluster -N $N -B $B -m 0.08 -m 10 -s 54321 -u $unit -C 5 -o ${NAME}COMs &> /dev/null
-#awk '{print "0.0000000000000001", $2, $3, $4, $5, $6, $7}' *COMs.dat.10 > dat.10
-#rm ${NAME}COMs.*
+Q=0.9
+./mcluster -N $Nc  -m 0.08 -m 10 -Q $Q -s 54321 -t 3 -u $unit -C 5 -o ${NAME}COMs &> /dev/null
+awk '{print "0.0000000000000001", $2, $3, $4, $5, $6, $7}' *COMs.dat.10 > dat.10
+rm ${NAME}COMs.*
 
 
 cat ${NAME}.dat.10 >> dat.10
