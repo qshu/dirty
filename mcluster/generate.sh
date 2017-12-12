a52ccabc4901921571b31a1f57db7cfe80866802
Qi <shuqi@pku.edu.cn>
2017-12-12 15:06:57 +0800

    set max mass to 100 Msun; add epoch = 25 Myr

--- mcluster/generate.sh
+++ mcluster/generate.sh
@@ -9,6 +9,7 @@ Nstar=12800 # Ns: total number of particles or stars(for ffc)
 B=0
 Nnomass=12800
 T=70000
+EPOCH=25  # Evolutionary epoch, in unit of Myr
 ((Ntot=Nstar+Nnomass))
 #echo $Ntot
 NAME=Nt${Ntot}_Ns${Nstar}_B${B}_Nn${Nnomass}_NBt_${T}
@@ -18,14 +19,14 @@ echo $NAME
 rm *.info *.input *.10
 
 
-./mcluster -N $Nstar -B $B -m 0.08 -m 1.4 -s 12345 -u $unit -C 5 -nbin 2 -T $T -o $NAME > log.gen
+./mcluster -N $Nstar -B $B -m 0.08 -m 100 -e 0 -s 12345 -u $unit -C 5 -nbin 2 -T $T -o $NAME > log.gen
 var=`awk '$1=="scalingInfo" {print $4, $7}' log.gen`
 echo $var
 #python aei.py $var $NAME
 
 echo 'first step finished!'
 
-./mcluster -N $Nnomass -B 0 -m 0.08 -m 1.4  -s 54321 -u $unit -C 5 -nbin 2 -T $T  > test.log.gen
+./mcluster -N $Nnomass -B 0 -m 0.08 -m 100  -s 54321 -u $unit -C 5 -nbin 2 -T $T  > test.log.gen
 #./mcluster -N 128000 -n 128000 -B 12800 -m 0.08 -m 10 -s 54321 -t 3 -u 0 -C 5 -T 100 > test.log.gen
 #exit
 awk 'BEGIN {k = sqrt(0.5/0.5)} { print $1 * 0.000000001, $2, $3, $4, $5 * k, $6 * k, $7 * k}' test.dat.10 >> ${NAME}.dat.10
